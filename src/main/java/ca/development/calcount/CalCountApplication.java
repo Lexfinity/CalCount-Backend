a0dad45c580f5c7d8967964ec97517d7f9ed803e
Ammar Rudani <aarudani7@gmail.com>
2019-05-09 23:31:52 -0400

    Initial Backend commit

---
+++ src/main/java/ca/development/calcount/CalCountApplication.java
@@ -0,0 +1,20 @@
+package ca.development.calcount;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication
+public class CalCountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalCountApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String greeting(){
+		return "Hello world!";
+	}
+}
---
+++ src/test/java/ca/development/calcount/CalCountApplicationTests.java
@@ -0,0 +1,16 @@
+package ca.development.calcount;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalCountApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
